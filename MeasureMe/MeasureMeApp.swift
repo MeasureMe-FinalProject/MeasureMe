d7f7b1e662603e08809dd74a45511184ff65dddd
Diki Dwi Diro <dikidwid0@gmail.com>
2024-03-02 20:14:51 +0700

    Initial Commit

---
+++ MeasureMe/MeasureMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeasureMeApp.swift
+//  MeasureMe
+//
+//  Created by Diki Dwi Diro on 02/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MeasureMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
